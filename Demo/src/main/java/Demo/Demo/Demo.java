a45ed925296036fd191168941f2b82e2eced8549
bhupendraghongade <bhupendraghongade@localhost>
2023-06-04 14:11:56 +0530

    bug fix

--- Demo/src/main/java/Demo/Demo/Demo.java
+++ Demo/src/main/java/Demo/Demo/Demo.java
@@ -8,9 +8,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Demo {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.guru99.com/");
-	
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 
 	}
 
